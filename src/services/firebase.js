c31ef95c091f7fad4a541734cace507bb9f1a10b
TombAnneX <109308405+AKTuminello@users.noreply.github.com>
2023-07-26 16:36:03 -0600

    file structure, imports, exports

---
+++ src/services/firebase.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import firebaseConfig   from './firebaseConfig';
+
+if(!firebase.apps.length){
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const db=firebase.firestore();
---
+++ src/services/firestore.js
@@ -0,0 +1,12 @@
+const firebaseConfig={
+
+  apiKey: 'AIzaSyC2NKbAfxvPPYIUwcYRVwegQtPjPurh7zU',
+  authDomain: 'sampler-platter.firebaseapp.com',
+  databaseURL: 'https://sampler-platter.firebaseio.com',
+  projectId: 'sampler-platter',
+  storageBucket: 'sampler-platter.appspot.com',
+  messagingSenderId: '1032704894314',
+  appId: '1:1032704894314:android:f02c858b2cec308e5d7113',
+};
+
+export default firebaseConfig;
